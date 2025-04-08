57cbc70a426be3a92f221d1e3c67c767c68dba41
Zengoozz <classicalegyptian@gmail.com>
2025-04-08 17:23:24 +0200

    NotFoundPage

--- eta.integrator.client/src/Pages/NotFoundPage.jsx
+++ eta.integrator.client/src/Pages/NotFoundPage.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from 'react';
+import { Button, Result, theme  } from 'antd';
+import { useNavigate } from 'react-router-dom';
 
 const NotFoundPage = () => {
-  return (
-    <div>NotFoundPage</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default NotFoundPage
+  const { token } = theme.useToken();
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        height: '100vh',
+        alignItems: 'center',
+        justifyContent: 'center',
+        background: token.colorBgLayout,
+        padding: '24px',
+      }}
+    >
+      <Result
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={
+          <Button type="primary" onClick={() => navigate('/')}>
+            Back Home
+          </Button>
+        }
+        style={{
+          background: token.colorBgContainer,
+          padding: '40px',
+          borderRadius: token.borderRadiusLG,
+          boxShadow: token.boxShadowSecondary,
+          color: token.colorText,
+        }}
+      />
+    </div>
+  );
+};
+
+export default NotFoundPage;
