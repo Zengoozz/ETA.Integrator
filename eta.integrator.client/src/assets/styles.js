39573e69a25c523d20ea9953cdbed72db95f71b9
Zengoozz <noureldeenelqady@gmail.com>
2025-04-09 00:49:05 +0200

    Styles>NavBar>

--- eta.integrator.client/src/assets/styles.js
+++ eta.integrator.client/src/assets/styles.js
@@ -1,14 +1,36 @@
 export const LoginContentStyle = {
-    padding: '0 48px',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
+   padding: "0 48px",
+   display: "flex",
+   justifyContent: "center",
+   alignItems: "center",
 };
 
 export const HomeContentStyle = {
-    padding: '0 48px',
-    display: 'flex',
-    justifyContent: 'start',
-    alignItems: 'start',
-    height: '500px'
+   padding: "0 48px",
+   display: "flex",
+   justifyContent: "start",
+   alignItems: "start",
+   height: "500px",
+};
+
+export const DarkNavbarTitleStyle = {
+   margin: 0,
+   color: "#000",
+};
+
+export const LightNavbarTitleStyle = {
+   margin: 0,
+   color: "#fff",
+};
+
+export const DarkNavbarIconStyle = {
+   fontSize: "20px",
+   color: "#000",
+   cursor: "pointer",
+};
+
+export const LightNavbarIconStyle = {
+    fontSize: "20px",
+    color: "#fff",
+    cursor: "pointer",
 };
