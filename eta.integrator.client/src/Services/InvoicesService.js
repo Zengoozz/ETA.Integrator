3bef4ce20efff0cdae3dedef73e41ad611aedca2
Zengoozz <classicalegyptian@gmail.com>
2025-04-14 17:37:18 +0200

    SubmitInvoices<ClientSide>

--- eta.integrator.client/src/Services/InvoicesService.js
+++ eta.integrator.client/src/Services/InvoicesService.js
@@ -4,7 +4,7 @@ const getInvoicesAccordingToDateAsQueryParams = async (values) => {
    try {
       const response = await GenericService.makeRequest(
          "GET",
-         `/Invoices?fromDate=${values.dateFrom}&toDate=${values.dateTo}`,
+         `/Invoices?fromDate=${values.dateFrom}&toDate=${values.dateTo}`
       );
       console.log(response);
       return response;
@@ -14,8 +14,19 @@ const getInvoicesAccordingToDateAsQueryParams = async (values) => {
    }
 };
 
-const submitInvoices = async (values) => {
-   console.log("submitInvoices", values);
-}
+const submitInvoices = async (invoices) => {
+   try {
+      const response = await GenericService.makeRequest(
+         "POST",
+         "/Invoices/Submit",
+         invoices
+      );
+      console.log(response);
+      return response;
+   } catch (error) {
+      console.error(error.message);
+      throw error;
+   }
+};
 
 export default { getInvoicesAccordingToDateAsQueryParams, submitInvoices };
