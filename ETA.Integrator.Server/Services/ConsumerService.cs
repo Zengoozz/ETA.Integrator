33ecdf95a2a8e1202937b7f04325b3b286116ec1
Zengoozz <classicalegyptian@gmail.com>
2025-05-15 11:42:20 +0300

    FixDate<ConsumerService/SubmitInvoice>

--- ETA.Integrator.Server/Services/ConsumerService.cs
+++ ETA.Integrator.Server/Services/ConsumerService.cs
@@ -152,7 +152,7 @@ namespace ETA.Integrator.Server.Services
             document.Signatures = new List<SignatureModel>();
             document.DocumentType = "i";
             document.DocumentTypeVersion = "0.9";
-            document.DateTimeIssued = trimmedUtcNow;
+            document.DateTimeIssued = trimmedUtcNow.AddMinutes(1);
             document.TaxpayerActivityCode = "8610"; // HOSPITAL ACTIVITIES CODE
             document.InternalID = invoiceViewModel.InvoiceId.ToString();
             document.InvoiceLines = invoiceViewModel.InvoiceItems;
