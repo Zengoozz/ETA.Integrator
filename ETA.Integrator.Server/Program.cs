43e3c615f502bb80dc0d5290b8a0acbb868ef1e1
Zengoozz <classicalegyptian@gmail.com>
2025-05-27 13:45:36 +0300

    AllowCors

--- ETA.Integrator.Server/Program.cs
+++ ETA.Integrator.Server/Program.cs
@@ -3,10 +3,22 @@ using ETA.Integrator.Server.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.WithOrigins("https://localhost:5173")
+               .AllowAnyMethod()
+               .AllowAnyHeader()
+               .AllowCredentials();
+    });
+});
+
 builder.Services.AddAllServices(builder.Configuration);
 
 var app = builder.Build();
 
+app.UseCors("AllowAll");
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
